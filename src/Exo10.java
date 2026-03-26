1be633e3aa15cb7db04c5bbd1efe48a0ff78b815
unknown <jeremie229242@gmail.com>
2026-03-26 18:39:20 +0100

    etape10

--- src/Exo10.java
+++ src/Exo10.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 public class Exo10 {
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args){
 
         // programme banque pour debutant
-        Scanner scanner = new Scanner(System.in);
+
 
         //declaration variables
-        double balance;
-        boolean isRunning;
+        double balance = 0;
+        boolean isRunning = true;
         int choix;
+
+        while (isRunning){
+
+
 
         // le menu
            System.out.println("*********************");
@@ -22,14 +28,17 @@ public class Exo10 {
         System.out.println("**************************");
         //processus de choix des utilisateurs
 
-        System.out.println("entrer votre choix (1-4): ");
+        System.out.print("entrer votre choix (1-4): ");
         choix = scanner.nextInt();
 
         switch (choix){
-            case 1 ->System.out.println("Voir balance");
-            case 2 ->System.out.println("Depot");
-            case 3 ->System.out.println("withdraw");
+            case 1 ->shwoBalance(balance);
+            case 2 ->balance= balance + depot();
+            case 3 ->balance = balance - withdraw(balance);
             case 4 -> isRunning = false ;
+            default -> System.out.println("choix invalide");
+        }
+
         }
         //balance
 
@@ -38,7 +47,42 @@ public class Exo10 {
         //withdraw
 
         //message quitter
+        System.out.println("**************************");
+        System.out.println("Merci et bonne journée");
+        System.out.println("**************************");
 
         scanner.close();
+    }
+
+    static void shwoBalance(double balance){
+        System.out.println("**************************");
+        System.out.printf("CFA%.2f\n", balance);
+    }
+    static double depot(){
+        double amount;
+        System.out.print("Entrer un montant pour le depot: ");
+
+        amount = scanner.nextDouble();
+        if (amount<0){
+            System.out.println("Pas un nombre negative");
+            return 0;
+        }
+        else {
+            return amount;
+        }
+    }
+    static double withdraw(double balance){
+        double amount;
+        System.out.print("Entrer le montant de tranfert: ");
+        amount = scanner.nextDouble();
+
+        if (amount>balance){
+            System.out.println("fond inssufisant");
+            return 0;
+        } else if (amount<0) {
+            System.out.println("pas de nombre negative");
+            return 0;
+        }
+        return amount;
     }
 }
